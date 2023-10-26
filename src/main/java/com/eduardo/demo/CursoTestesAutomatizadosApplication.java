a4644b6e2c28477532552cfa0ed76366e63cc4c6
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-10-26 20:16:59 -0300

    first commit

---
+++ src/main/java/com/eduardo/demo/CursoTestesAutomatizadosApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestesAutomatizadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTestesAutomatizadosApplication.class, args);
+	}
+
+}
