2bf12cd81efc77393d8ed190ab9602a56ae8af78
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-10-27 20:12:08 -0300

    Testes otimizados sem spring boot

--- src/test/java/com/eduardo/demo/domain/PlanetServiceTest.java
+++ src/test/java/com/eduardo/demo/domain/PlanetServiceTest.java
@@ -1,19 +1,29 @@
 package com.eduardo.demo.domain;
 
+import static com.eduardo.demo.common.PlanetConstants.INVALID_PLANET;
 import static com.eduardo.demo.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
-    @Autowired
+   // @Autowired
+    @InjectMocks
     private PlanetService planetService;
-    @MockBean
+   // @MockBean
+    @Mock
     private PlanetRepository planetRepository;
 
     @Test
@@ -26,5 +36,12 @@ public class PlanetServiceTest {
        Planet sut = planetService.create(PLANET);
        ///Assert
        assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+       assertThatThrownBy (() ->  planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
