f67552c90d77f8d5e3cc7052e26bf3dd7cf31b15
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-10-28 21:49:50 -0300

    Testando cenario de sucesso e de dados invalidos no controller

--- src/test/java/com/eduardo/demo/web/PlanetControllerTest.java
+++ src/test/java/com/eduardo/demo/web/PlanetControllerTest.java
@@ -1,4 +1,55 @@
 package com.eduardo.demo.web;
 
+import static com.eduardo.demo.common.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.eduardo.demo.domain.Planet;
+import com.eduardo.demo.domain.PlanetService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PlanetController.class)
 public class PlanetControllerTest {
-}
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidateData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void createPlanete_WithInvalidData_ReturnsBadRequest() throws Exception {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("","","");
+
+        mockMvc.
+                perform(post("/planets").content(objectMapper.writeValueAsString(emptyPlanet))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity());
+
+        mockMvc.
+                perform(post("/planets").content(objectMapper.writeValueAsString(invalidPlanet))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity());
+    }
+    }
